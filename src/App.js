146c8c1d5eeb398df7b7bd4bbd7877e2ea9f089b
JandersonCRB <jandersonangelo@hotmail.com>
2018-09-27 21:54:21 -0300

    Added modal

--- src/App.js
+++ src/App.js
@@ -19,29 +19,16 @@ const initFirebase = () => {
 };
 
 class App extends Component {
-  arts = [
-    { name: 'Rainbow',        value: 'rainbow' },
-    { name: 'Blues',          value: 'blues' },
-    { name: 'Superhero',      value: 'superhero' },
-    { name: 'Radial',         value: 'radial' },
-    { name: 'Tilt',           value: 'tilt' },
-    { name: 'Purple',         value: 'purple' },
-    { name: 'Horizon',        value: 'horizon' },
-    { name: 'Italic Outline', value: 'italic-outline' },
-    { name: 'Slate',          value: 'slate' },
-  ];
   constructor(){
     super();
     this.state = {
       arts: [],
-      currentArt: this.arts[0].value
     }
   }
 
   componentWillMount(){
     initFirebase();
     this.listenToArts();
-    this.text = React.createRef();
   }
 
   listenToArts() {
@@ -59,21 +46,6 @@ class App extends Component {
     })
   }
 
-  submit = (e) => {
-    e.preventDefault();
-    let db = firebase.database();
-    const { currentArt } = this.state;
-    const text = this.text.current.value;
-    if(text.length >= 3){
-        db.ref('arts/').push({
-          text: text,
-          art: currentArt
-        });
-    } else {
-        alert("O texto precisa ter pelo menos 3 caracteres.")
-    }
-  };
-
   delete = key => {
       let db = firebase.database();
       db.ref('arts/' + key).remove();
@@ -85,33 +57,28 @@ class App extends Component {
               <button type="button" className="close" aria-label="Close" style={{ color: 'red'}} onClick={ () => this.delete(art.key) }>
                   <span aria-hidden="true">&times;</span>
               </button>
+              <button type="button" className="close" aria-label="Close" data-toggle="modal" data-target="#exampleModal" style={{ color: 'red'}} onClick={ () => this.setState({editArt: art}) }>
+                  <span aria-hidden="true">&times;</span>
+              </button>
               <div className={ `wordart ${art.art}` } >
                   <span className="text" style={{fontSize: '60%'}}>{ art.text }</span>
               </div>
           </div>
       ))
   );
-  changeArt(e) {
-    this.setState({ currentArt: e.target.value })
-  }
+
   render() {
     return (
       <div className="App">
+        <EditBox art={this.state.editArt} />
         <header className="App-header">
+          <button type="button" className="close" data-toggle="modal" data-target="#exampleModal" style={{ color: 'green'}} onClick={() => this.setState({editArt: undefined}) }>
+            <span aria-hidden="true">&times;</span>
+          </button>
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Firebase</h1>
         </header>
         <div className="container">
-          <form onSubmit={this.submit.bind(this)} className="App-intro">
-            <input type="text" placeholder="Texto" maxLength={10} ref={this.text}/> <br />
-
-            <select value={this.state.currentArt} onChange={this.changeArt.bind(this)} >
-              { this.arts.map((art, key) => (
-                  <option value={art.value} key={key} >{art.name}</option>
-              )) }
-            </select>
-            <input type="submit" value="Enviar"/>
-          </form>
           <div className="row">
             <this.renderArts />
           </div>
@@ -119,6 +86,123 @@ class App extends Component {
       </div>
     );
   }
+}
+
+class EditBox extends Component {
+    arts = [
+        { name: 'Rainbow',        value: 'rainbow' },
+        { name: 'Blues',          value: 'blues' },
+        { name: 'Superhero',      value: 'superhero' },
+        { name: 'Radial',         value: 'radial' },
+        { name: 'Tilt',           value: 'tilt' },
+        { name: 'Purple',         value: 'purple' },
+        { name: 'Horizon',        value: 'horizon' },
+        { name: 'Italic Outline', value: 'italic-outline' },
+        { name: 'Slate',          value: 'slate' },
+    ];
+
+    constructor(props){
+        super(props);
+        this.text = React.createRef();
+        this.state = {
+            currentArt: this.arts[0].value,
+        };
+
+    }
+
+    componentWillReceiveProps(props) {
+        if(props.art){
+            this.text.current.value = props.art.text;
+            this.setState({ currentArt: props.art.art});
+        } else {
+            this.text.current.value = "";
+            this.setState({ currentArt: this.arts[0].value })
+        }
+    }
+
+
+    changeArt(e) {
+        this.setState({ currentArt: e.target.value })
+    }
+
+    reset() {
+        this.text.current.value = "";
+        this.setState({ currentArt: this.arts[0].value });
+    }
+
+    submit = () => {
+        if (this.props.art){
+            this.update(this.props.art.key);
+        } else {
+            this.create();
+        }
+        this.reset();
+    };
+
+    create() {
+        let db = firebase.database();
+        const { currentArt } = this.state;
+        const text = this.text.current.value;
+        if(text.length >= 3){
+            db.ref('arts/').push({
+                text: text,
+                art: currentArt
+            });
+        } else {
+            alert("O texto precisa ter pelo menos 3 caracteres.")
+        }
+    }
+
+    update() {
+        let db = firebase.database();
+        db.ref('arts/' + this.props.art.key).set({
+            art: this.state.currentArt,
+            text: this.text.current.value
+        })
+
+    }
+
+    render() {
+        console.log(this.text)
+        return(
+            <div className="modal" tabIndex="-1" role="dialog" id="exampleModal">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Modal title</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <form className="App-intro">
+                            <div className="modal-body">
+                                <div className="form-group">
+                                    <input type="text" className="form-control" placeholder="Texto" maxLength={10} ref={this.text}/> <br />
+                                </div>
+                                <div className="form-group">
+                                    <select value={this.state.currentArt} className="form-control" onChange={this.changeArt.bind(this)} >
+                                        { this.arts.map((art, key) => (
+                                            <option value={art.value} key={key} >{art.name}</option>
+                                        )) }
+                                    </select>
+                                </div>
+                                <div>
+                                    <h4>Preview</h4>
+                                    <div className={ `wordart ${this.state.currentArt}` }>
+                                        <span className="text" style={{fontSize: '60%'}}>{ this.text.current.value }</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="submit" className="btn btn-primary" data-dismiss="modal" onClick={this.submit.bind(this)}>Save changes</button>
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
